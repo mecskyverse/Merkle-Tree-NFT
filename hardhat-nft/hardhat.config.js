a7f0643ef089317c9cd3bc48eaf8d078b771e5c4
Aakash <vermaaakash1000@gmail.com>
2022-09-23 16:33:19 +0530

    Goerli Network configuration added

--- hardhat-nft/hardhat.config.js
+++ hardhat-nft/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+const GoerliApiKey = process.env.Goerli_Api_Key;
+const GoerliPrivateKey = process.env.Goerli_Private_Key;
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: GoerliApiKey,
+      accounts: [GoerliPrivateKey],
+    },
+  },
 };
