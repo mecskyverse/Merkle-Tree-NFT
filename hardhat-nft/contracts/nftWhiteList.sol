88183bb537be004578cfda570d65ed870720c117
Aakash <vermaaakash1000@gmail.com>
2022-09-23 11:06:54 +0530

    Smart contract added

---
+++ hardhat-nft/contracts/nftWhiteList.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MyToken is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    bytes32 public root;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(bytes32 _root) ERC721("NFT", "MTK") {
+        root = _root;
+    }
+
+    function safeMint(address to, bytes32[] memory proof) public {
+        require(
+            isValid(proof, keccak256(abi.encodePacked(msg.sender))),
+            "Not a part of Allowlist"
+        );
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function isValid(bytes32[] memory proof, bytes32 leaf)
+        internal
+        view
+        returns (bool)
+    {
+        return MerkleProof.verify(proof, root, leaf);
+    }
+}
